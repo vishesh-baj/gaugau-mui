e6cb7d9bd53c13a7e2d0fef66166e9781fe1fb81
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-06-07 15:54:16 +0530

    added table for customer section with get api

--- client/src/components/Tables/CustomerTable.jsx
+++ client/src/components/Tables/CustomerTable.jsx
@@ -1,19 +1,144 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import DataTable from "react-data-table-component";
-const CustomerTable = ({ columns, data }) => {
+import { Button, Stack } from "@mui/material";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "../../routes/paths";
+import CreateIcon from "@mui/icons-material/Create";
+import DeleteIcon from "@mui/icons-material/Delete";
+const CustomerTable = () => {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleEditClick = (row) => {
+    navigate(PATHS.customerDetails);
+    console.log("EDIT ROW: ", row);
+  };
+
+  const handleDeleteClick = (row) => {
+    console.log("DELTE ROW: ", row);
+  };
+
+  const getCountries = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3030/api/admin/customerList"
+      );
+      console.log(response.data.Message);
+      setCountries(response.data.Message);
+      setFilteredCountries(response.data.Message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  useEffect(() => {
+    const result = countries.filter((country) => {
+      return country.name.toLowerCase().match(search.toLowerCase());
+    });
+    setFilteredCountries(result);
+  }, [search]);
+
+  const columns = [
+    { name: "S.No", selector: (row) => row.id, sortable: true },
+    {
+      name: "Name",
+      selector: (row) => row.first_name + " " + row.last_name,
+      sortable: true,
+    },
+
+    {
+      name: "Mobile",
+      selector: (row) => row.mobile_number,
+      sortable: true,
+    },
+    {
+      name: "No. Of Cattles",
+      selector: (row) => row.number_of_cattle_to_buy,
+      sortable: true,
+    },
+    {
+      name: "Client",
+      selector: (row) =>
+        row.campaign_customersref[0] ? (
+          row.campaign_customersref[0].clientref.client_name
+        ) : (
+          <span>empty</span>
+        ),
+      sortable: true,
+    },
+    {
+      name: "Campaign Title",
+      selector: (row) =>
+        row.campaign_customersref[0] ? (
+          row.campaign_customersref[0].campaignsref.campaign_name
+        ) : (
+          <span>empty</span>
+        ),
+      sortable: true,
+    },
+    {
+      name: "State",
+      selector: (row) => row.stateref.name,
+      sortable: true,
+    },
+    {
+      name: "District",
+      selector: (row) => row.districtref.district,
+      sortable: true,
+    },
+    {
+      name: "Tehsil",
+      selector: (row) => row.tehsilref.tahshil,
+      sortable: true,
+    },
+    {
+      name: "Actions",
+      cell: (row) => (
+        <Stack direction={"row"} spacing={2}>
+          <CreateIcon
+            style={{ cursor: "pointer" }}
+            color="primary"
+            onClick={() => handleEditClick(row)}
+          />
+          <DeleteIcon
+            style={{ cursor: "pointer" }}
+            color="error"
+            onClick={() => handleDeleteClick(row)}
+          />
+        </Stack>
+      ),
+    },
+  ];
+
   return (
     <div>
       <DataTable
         title="Countries List"
         columns={columns}
-        data={data}
+        data={filteredCountries}
         fixedHeader
         fixedHeaderScrollHeight="450px"
         pagination
         highlightOnHover
         responsive
-
-        // use action to add upload csv in campaign
+        subHeader
+        subHeaderComponent={
+          <input
+            type="text"
+            placeholder="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        }
       />
     </div>
   );
--- client/src/pages/Customers.jsx
+++ client/src/pages/Customers.jsx
@@ -1,66 +1,6 @@
 import CustomerTable from "../components/Tables/CustomerTable";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { Button } from "@mui/material";
-const Table = () => {
-  const [countries, setCountries] = useState([]);
-  const getCountries = async () => {
-    try {
-      const response = axios.get("https://restcountries.com/v2/all");
-      setCountries((await response).data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getCountries();
-  }, []);
-
-  const handleEditClick = (row) => {
-    console.log("EDIT ROW: ", row);
-  };
-
-  const handleDeleteClick = (row) => {
-    console.log("DELTE ROW: ", row);
-  };
-
-  const columns = [
-    { name: "Country Name", selector: (row) => row.name, sortable: true },
-    {
-      name: "Country Native Name",
-      selector: (row) => row.nativeName,
-      sortable: true,
-    },
-    { name: "Country Capital", selector: (row) => row.capital, sortable: true },
-    {
-      name: "Country Flag",
-      selector: (row) => <img width={50} height={50} src={row.flag} />,
-    },
-    {
-      name: "Actions",
-      cell: (row) => (
-        <>
-          <Button
-            onClick={() => handleEditClick(row)}
-            variant="text"
-            color="info"
-          >
-            Edit
-          </Button>
-          <Button
-            onClick={() => handleDeleteClick(row)}
-            variant="text"
-            color="warning"
-          >
-            Delete
-          </Button>
-        </>
-      ),
-    },
-  ];
-
-  return <CustomerTable columns={columns} data={countries} />;
+const Customers = () => {
+  return <CustomerTable />;
 };
 
-export default Table;
+export default Customers;
